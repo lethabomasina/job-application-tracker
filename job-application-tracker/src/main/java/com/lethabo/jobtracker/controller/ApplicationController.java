95642911f1f4ced1358e87414e457815661bdc1d
Lethabo Masina <296612561+lethabomasina@users.noreply.github.com>
2026-09-19 14:10:11 +0200

    Add GET by ID and DELETE endpoints

--- job-application-tracker/src/main/java/com/lethabo/jobtracker/controller/ApplicationController.java
+++ job-application-tracker/src/main/java/com/lethabo/jobtracker/controller/ApplicationController.java
@@ -2,12 +2,17 @@ package com.lethabo.jobtracker.controller;
 
 import com.lethabo.jobtracker.model.Application;
 import com.lethabo.jobtracker.service.ApplicationService;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ApplicationController {
@@ -18,8 +23,13 @@ public class ApplicationController {
     }
 
     @GetMapping("/applications")
-    public List<Application> getApplication() {
+    public List<Application> getApplications() {
         return applicationService.getApplications();
+    }
+
+    @GetMapping("/applications/{id}")
+    public Optional<Application> getApplication(@PathVariable Long id) {
+        return applicationService.getApplication(id);
     }
 
     @PostMapping("/applications")
@@ -27,5 +37,13 @@ public class ApplicationController {
         Application createdApplication = applicationService.createApplication(application);
 
         return ResponseEntity.status(201).body(createdApplication);
+    }
+
+    @DeleteMapping("/applications/{id}")
+    public ResponseEntity<Void> deleteApplication(@PathVariable Long id) {
+        if (applicationService.deleteApplication(id)) {
+            return ResponseEntity.status(204).build();
+        }
+        return ResponseEntity.status(404).build();
     }
 }
--- job-application-tracker/src/main/java/com/lethabo/jobtracker/service/ApplicationService.java
+++ job-application-tracker/src/main/java/com/lethabo/jobtracker/service/ApplicationService.java
@@ -4,6 +4,7 @@ import com.lethabo.jobtracker.model.Application;
 import com.lethabo.jobtracker.repository.ApplicationRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ApplicationService {
@@ -17,7 +18,20 @@ public class ApplicationService {
         return applicationRepository.findAll();
     }
 
+    public Optional<Application> getApplication(Long id) {
+        return applicationRepository.findById(id);
+    }
+
     public Application createApplication(Application application) {
         return applicationRepository.save(application);
+    }
+
+    public boolean deleteApplication(Long id) {
+        if (applicationRepository.findById(id).isPresent()) {
+            applicationRepository.deleteById(id);
+            return true;
+        }
+
+        return false;
     }
 }
